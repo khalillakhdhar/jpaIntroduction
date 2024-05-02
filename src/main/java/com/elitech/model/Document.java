6fcb3114056ba9eb2bb746903302c50a0ac1c1a7
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2024-05-02 23:33:30 +0100

    app

---
+++ src/main/java/com/elitech/model/Document.java
@@ -0,0 +1,41 @@
+package com.elitech.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Document {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable = false)
+	private String titre;
+	@Column(nullable = false)
+	private String url;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
+	
+	
+}
--- src/main/java/com/elitech/model/Student.java
+++ src/main/java/com/elitech/model/Student.java
@@ -2,11 +2,14 @@ package com.elitech.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Student {
 @Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
 private long id;
 @Column(nullable = false,length = 30)
 private String nom;
